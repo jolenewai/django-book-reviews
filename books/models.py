4fa7a68f2c8c05890c1b4e5b2ce210d2507391f8
jolenewai <jolene.wai@gmail.com>
2020-05-04 07:18:41 +0000

    added models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(blank=False, max_length=255)
+    ISBN = models.CharField(blank=False, max_length=255)
+    desc = models.TextField(blank=False)
+
+    def __str__(self):
+        return self.title
+
+class Author(models.Model):
+    first_name = models.CharField(blank=False, max_length=35)
+    last_name = models.CharField(blank=False, max_length=35)
+    dob = models.DateField(blank=True)
+
+    def __str__(self):
+        author_name = self.first_name + ' ' + self.last_name
+        return author_name
